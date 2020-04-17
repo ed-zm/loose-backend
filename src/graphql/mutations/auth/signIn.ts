508ffed03382744e9b569d7e194362a1cb8ae407
Eduardo Zarraga <eduardoandres8391@hotmail.com>
2020-04-17 01:56:36 -0400

    Remove loose-components

--- src/graphql/mutations/auth/signIn.ts
+++ src/graphql/mutations/auth/signIn.ts
@@ -1,14 +1,13 @@
 import { compareSync } from 'bcrypt'
 import { sign } from 'jsonwebtoken'
 import { stringArg, booleanArg } from 'nexus'
-import errors from 'loose-components/src/utils/errors'
 
 const resolve = async (_, { email, password, staySignedIn }, ctx, info) => {
   const user = await ctx.prisma.user({ email }, '{ id, hash, emailVerifiedAt }')
-  if(!user) throw new Error(errors.EMAIL_NOT_FOUND)
-  if(!user.hash) throw new Error(errors.NO_PASSWORD)
-  if(!user.emailVerifiedAt) throw new Error(errors.CONFIRM_EMAIL)
-  if(!compareSync(password, user.hash)) throw new Error(error.INVALID_PASSWORD)
+  if(!user) throw new Error("Email not found")
+  if(!user.hash) throw new Error("No Password set")
+  if(!user.emailVerifiedAt) throw new Error("'You have to confirm your Email")
+  if(!compareSync(password, user.hash)) throw new Error("Invalid Credentials")
   const token = sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: staySignedIn ? '1y':'30d' })
   return token
 }
