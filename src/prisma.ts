89d1b6c830498f7d3115a348bd3c79d6806ea6cd
Eduardo Zarraga <eduardoandres8391@hotmail.com>
2020-04-20 23:27:33 -0400

    add production endpoint

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,6 +1,8 @@
 import { Prisma } from '../generated/prisma-client'
 
 export default new Prisma({
-    endpoint: `${process.env.PRISMA_HOST}:${process.env.PRISMA_PORT}/${process.env.PRISMA_SERVICE}/${process.env.STAGE}`,
+    endpoint: process.env.NODE_ENV === 'production' ?
+    `${process.env.PRISMA_HOST}:${process.env.PRISMA_PORT}/prisma` :
+    `${process.env.PRISMA_HOST}:${process.env.PRISMA_PORT}/${process.env.PRISMA_SERVICE}/${process.env.STAGE}`,
     secret: process.env.PRISMA_SECRET
   })
