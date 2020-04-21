13f3fcfb2beae59ccba49acf8c15bde31edf0c3f
Eduardo Zarraga <eduardoandres8391@hotmail.com>
2020-04-20 23:19:22 -0400

    add new endpoint

--- src/helpers/endpoint.ts
+++ src/helpers/endpoint.ts
@@ -1,1 +1,1 @@
-export default () => process.env.NODE_ENV === 'production' ? 'http://loose.dev' : 'http://alpha.loose.dev'
+export default () => process.env.NODE_ENV === 'production' ? 'http://loose.dev/api' : 'http://alpha.loose.dev/api'
