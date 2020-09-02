ee0425d03c3d48aeaffcc5bd07afb033c38f6315
Eduardo Zarraga <eduardoandres8391@hotmail.com>
2020-09-01 21:43:38 -0400

    change to DO

--- src/graphql/queries/aws/getS3SignedUrl.ts
+++ src/graphql/queries/aws/getS3SignedUrl.ts
@@ -1,21 +1,25 @@
-import { S3 } from 'aws-sdk'
+import { S3, Endpoint } from 'aws-sdk'
 import { stringArg, idArg, booleanArg } from 'nexus'
 import uid from 'uid'
 import authenticate from '../../../helpers/authenticate'
 
-const resolve = async ({ args: { fileType, id, operation, random }, ctx, user }) => {
+const resolve = async ({ args: { fileType, id, operation, random, folder = 'attachments' }, ctx, user }) => {
+  const spacesEndpoint = new Endpoint(`${process.env.DO_SPACES_REGION}.digitaloceanspaces.com`)
   const s3 = new S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION
+    endpoint: spacesEndpoint,
+    accessKeyId: process.env.DO_SPACES_ID,
+    secretAccessKey: process.env.DO_SPACES_SECRET,
+    region: process.env.DO_SPACES_REGION,
+    signatureVersion: 'v4'
   })
   const extension = fileType.split('/')
   const key = `${random ? uid(10) : id}.${extension[1]}`
   const params = {
-    Bucket: `dev.loose.www.avatars`,
+    Bucket: `${process.env.DO_SPACES_NAME}/${folder}`,
     Key: key,
     Expires: 600,
-    ContentType: fileType
+    ContentType: fileType,
+    ACL: 'public-read'
   }
   const url = await s3.getSignedUrl(operation, params)
   return url
@@ -27,7 +31,8 @@ const resolve = async ({ args: { fileType, id, operation, random }, ctx, user })
       fileType: stringArg({ nullable: false }),
       id: idArg({ nullable: false }),
       operation: stringArg({ nullable: false }),
-      random: booleanArg({ nullable: true })
+      random: booleanArg({ nullable: true }),
+      folder: stringArg({ nullable: true })
     },
     nullable: false,
     resolve: async (_, args, ctx, info) => await authenticate({ args, ctx, info, resolve })
