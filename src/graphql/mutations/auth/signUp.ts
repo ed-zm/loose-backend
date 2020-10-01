22a2f2e8a6ee30c4b82a5ed5cdb71039f02bbc38
Eduardo Zarraga <eduardoandres8391@hotmail.com>
2020-10-01 03:13:41 -0400

    add stripe

--- src/graphql/mutations/auth/signUp.ts
+++ src/graphql/mutations/auth/signUp.ts
@@ -1,11 +1,12 @@
 import { hashSync } from 'bcrypt'
+import Stripe from 'stripe'
 import moment from 'moment'
 import { sign } from 'jsonwebtoken'
 import { stringArg } from '@nexus/schema'
 import { sendEmail } from '../../../helpers/email'
 import uid from 'uid'
 
-const resolve = async (_: any, { email, password, firstName, lastName, username, inviteCode }: any, ctx: any) => {
+const resolve = async (_: any, { email, password, firstName, lastName, username, inviteCode, stripeToken, subscription }: any, ctx: any) => {
     const exists = await ctx.prisma.user.findOne({
       where: {
         email
@@ -18,12 +19,28 @@ const resolve = async (_: any, { email, password, firstName, lastName, username,
         email
       }
     })
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+    const subscriptionType = process.env.STRIPE_STANDARD_SUBSCRIPTION
+    const customer = await stripe.customers.create({
+      email,
+      source: stripeToken
+    })
+    const subscriptionObj = await stripe.subscriptions.create({
+      customer: customer.id,
+      items: [{
+        price: subscriptionType
+      }]
+    })
+    console.log('DOWN', subscription)
     const isInvited = invited && !!invited.length
     const emailVerificationCode = uid(16)
     const hash = hashSync(password, 10)
+    console.log('CREATE', customer.id, subscriptionObj.id)
     const user = await ctx.prisma.user.create({
       data: {
         email,
+        stripeId: customer.id,
+        stripeSubscriptionId: subscriptionObj.id,
         username,
         hash,
         firstName,
@@ -49,7 +66,9 @@ export default {
     firstName: stringArg({ nullable: false }),
     lastName: stringArg({ nullable: true }),
     username: stringArg({ nullable: false }),
-    inviteCode: stringArg({ nullable: true })
+    inviteCode: stringArg({ nullable: true }),
+    stripeToken: stringArg({ nullable: false }),
+    subscription: stringArg({ nullable: false })
   },
   nullable: false,
   resolve
